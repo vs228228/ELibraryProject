6f7ba54d69909c291e219dc2e5d5e5ca8ba504a5
lunarKettle <1evgeny.nesterenko1@gmail.com>
2024-02-06 12:34:29 +0300

    add book class

---
+++ ELibraryProject/Classes/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELibraryProject.Classes
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+        public int WritingDate { get; set; }
+    }
+}
